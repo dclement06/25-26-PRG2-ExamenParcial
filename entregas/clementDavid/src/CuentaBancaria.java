6e5d6c0e26085611bf613a609883f3ad7c0f8297
dclement06 <david.clement@alumnos.uneatlantico.es>
2026-04-06 23:07:41 +0200

    feat: añadir constructor publico usando encapsulamiento this()

--- entregas/clementDavid/src/CuentaBancaria.java
+++ entregas/clementDavid/src/CuentaBancaria.java
@@ -13,3 +13,6 @@ private CuentaBancaria(String titular, double saldo, String iban, TipoCuenta tip
     this.iban = iban;
     this.tipo = tipo;
 }
+public CuentaBancaria(String titular, double saldoInicial) {
+    this(titular, saldoInicial, "ES-PENDIENTE", TipoCuenta.ESTANDAR);
+}
