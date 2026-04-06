b3c435409743e97874488140cfa2061dd8e58483
dclement06 <david.clement@alumnos.uneatlantico.es>
2026-04-06 23:04:40 +0200

    feat: implementar constructor de Cliente con asserts de integridad

--- entregas/clementDavid/src/Cliente.java
+++ entregas/clementDavid/src/Cliente.java
@@ -3,3 +3,10 @@ public class Cliente {
     private String dni;
     private String direccion;
 }
+public Cliente(String nombre, String dni, String direccion) {
+    assert nombre != null;
+    assert dni != null;
+    this.nombre = nombre;
+    this.dni = dni;
+    this.direccion = direccion;
+}
