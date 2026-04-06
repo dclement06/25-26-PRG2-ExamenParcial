abdca02bb2318da820062c61ad780d677a535267
dclement06 <david.clement@alumnos.uneatlantico.es>
2026-04-06 23:18:50 +0200

    feat: crear Main e inicializar Banco y Clientes

---
+++ entregas/clementDavid/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Banco central = new Banco("Banco Central");
+        Cliente ana = new Cliente("Ana García", "12345678A", "Calle Falsa 123");
+        Cliente juan = new Cliente("Juan Pérez", "87654321B", "Avenida Siempre Viva");
+    }
+}
