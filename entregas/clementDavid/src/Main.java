27c094a101bfead3d2316ac821a60479b8886f85
dclement06 <david.clement@alumnos.uneatlantico.es>
2026-04-06 23:20:53 +0200

    feat: instanciar cuentas

--- entregas/clementDavid/src/Main.java
+++ entregas/clementDavid/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Banco central = new Banco("Banco Central");
-        Cliente ana = new Cliente("Ana García", "12345678A", "Calle Falsa 123");
-        Cliente juan = new Cliente("Juan Pérez", "87654321B", "Avenida Siempre Viva");
+        Cliente ana = new Cliente("Ana García", "12345678A", "Calle Albert Einstein 16");
+        Cliente juan = new Cliente("Juan Pérez", "87654321B", "Avenida Principal 62");
     }
 }
+CuentaBancaria c1 = new CuentaBancaria("Ana García", 5000);
+CuentaBancaria c2 = new CuentaBancaria("Juan Pérez", 3000);
